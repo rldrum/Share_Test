ddfe4296fa49dfeafb751aa2c9ef1c437bbe9285
rldrum <rldrum@gmail.com>
2015-06-14 16:50:36 +0900

    몇가지수정

---
+++ Assets/test2.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		transform.rotation = new Quaternion (90, 0, 0, 0);
+	
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
